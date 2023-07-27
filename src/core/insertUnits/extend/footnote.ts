6d4a0370448491e185dd4b4feb88a0df0b7921eb
Potmot <377029227@qq.com>
2023-07-28 02:22:23 +0800

    2023/7/28 调整footnote插入单元

--- src/core/insertUnits/extend/footnote.ts
+++ src/core/insertUnits/extend/footnote.ts
@@ -1,5 +1,8 @@
-import {InsertUnit} from "../../../declare/InsertUtil";
-import {simpleInsert} from "../../../utils/editor/insertUtils";
+import {InsertUnit, OptionInsertArgument} from "../../../declare/InsertUtil";
+import {formatInsert} from "../../../utils/editor/insertUtils";
+import {ref} from "vue";
+
+const footnoteTypeOptions = ["行内", "行外"]
 
 export const footnote: InsertUnit = {
     triggers: [
@@ -10,14 +13,44 @@ export const footnote: InsertUnit = {
     ],
     label: "脚标",
     insert: (args, textarea) => {
-        return simpleInsert(
+        const type = args.get('footnoteType')!.value
+        return formatInsert(
             textarea,
-            "footnote",
-            "[^",
-            "]"
-        );
+            "code",
+            (startPart, midPart, endPart) => {
+                if (type == footnoteTypeOptions[0]) {
+                    return {
+                        content: [startPart, '^[', midPart, ']', endPart],
+                        start: startPart.length + 2 + midPart.length + 1,
+                    }
+                }
+                const ref = `[^${midPart}]`
+                const tail = `[^${midPart}]: `
+                if (midPart.length > 0) {
+                    return {
+                        content: [startPart, ref, '\n', tail, '\n', endPart],
+                        start: startPart.length + ref.length + 1 + tail.length,
+                    }
+                } else {
+                    return {
+                        content: [startPart, ref, '\n', tail, '\n', endPart],
+                        start: startPart.length + 2,
+                    }
+                }
+            }
+        )
     },
-    arguments: [],
+    arguments: [
+        <OptionInsertArgument>{
+            name: "footnoteType",
+            label: "类型",
+            getRef: () => {
+                let footnoteType = "行内";
+                return ref(footnoteType);
+            },
+            options: footnoteTypeOptions
+        }
+    ],
     reject: true,
     prevent: true,
 }
